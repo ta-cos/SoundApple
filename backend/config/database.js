efc639a80c70c503cce12611063742d1f9995bb0
Nathan Treadaway <natetread@gmail.com>
2022-01-10 16:32:35 -0500

    fixed database.js

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,6 +1,10 @@
-const {
-  db: { username, password, database, host },
-} = require("./index");
+const config = require('./index');
+
+const db = config.db;
+const username = db.username;
+const password = db.password;
+const database = db.database;
+const host = db.host
 
 module.exports = {
   development: {
@@ -9,10 +13,17 @@ module.exports = {
     database,
     host,
     dialect: 'postgres',
+    seederStorage: 'sequelize',
   },
   production: {
     use_env_variable: 'DATABASE_URL',
     dialect: 'postgres',
     seederStorage: 'sequelize',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      }
+    }
   }
-};
+}

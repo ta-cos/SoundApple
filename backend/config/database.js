06f4a566b736de6943f0914932b6f7052795c8ae
Nathan Treadaway <natetread@gmail.com>
2022-01-09 22:26:53 -0500

    reverted changes

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,25 +1,14 @@
-const server = {
-  "development": {
-    "username": "root",
-    "password": null,
-    "database": "database_development",
-    "host": "127.0.0.1",
-    "dialect": "mysql",
-    "operatorsAliases": false
+module.exports = {
+  development: {
+    username,
+    password,
+    database,
+    host,
+    dialect: 'postgres',
   },
-  "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql",
-    "operatorsAliases": false
-  },
-  "dialect": "postgres",
-  "seederStorage": "sequelize",
-  "production": {
-    "use_env_variable": 'DATABASE_URL',
-    "dialect": 'postgres',
-    "seederStorage": 'sequelize',
+  production: {
+    use_env_variable: 'DATABASE_URL',
+    dialect: 'postgres',
+    seederStorage: 'sequelize',
   }
 };
